0f3374510d13c1101af927dbff300aef4a79af86
Sahil Verma <iamsahil1910@gmail.com>
2021-12-09 09:09:36 +0530

    flask app 2 html

--- app.py
+++ app.py
@@ -1,23 +1,102 @@
-from flask import Flask, request
-from flask.templating import render_template
+import os
+from flask import Flask, request, render_template, redirect, session
+from flask_session import Session
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+from datetime import datetime
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 
+# Configure session to use filesystem
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+UPLOAD_FOLDER = '/static/images/'
+
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
+
+# Set up database
+engine = create_engine("postgresql:///temp")
+db = scoped_session(sessionmaker(bind=engine))
 
 @app.route('/')
 def hello():
-    return render_template('index.html')
 
-@app.route('/login')
+    blogs = db.execute("SELECT * FROM blog;").fetchall()
+    print(blogs)
+
+    return render_template('index.html', blogs=blogs)
+
+@app.route("/login", methods=['GET', 'POST'])
 def login():
-    return render_template('login.html')
-    
 
-@app.route('/admin')
+    session.clear()
+
+    if request.method == "GET":
+
+        return render_template("login.html")
+
+    else:
+
+        if not request.form.get("username"):
+            return render_template("login.html", message = "Username Missing")
+        if not request.form.get("password"):
+            return render_template("login.html", message = "Password Missing")
+
+        row = db.execute("SELECT * FROM admin WHERE username = :username AND password = :password" , {'username': request.form.get("password"), 'password': request.form.get("username")}).fetchone()
+        
+        if row == None:
+            return render_template('login.html', message = 'Wrong Username or Password')
+        session["user_id"] = row["user_id"]
+
+        return redirect("/admin")
+
+
+@app.route('/logout')
+def logout():
+    session.clear()
+
+    return redirect('/login')
+
+@app.route('/admin', methods=['GET', 'POST'])
 def admin():
-    return render_template('admin.html')
 
+    if session.get("user_id") is None:
 
+        return redirect('/login')
+
+    if request.method == "GET":
+
+        return render_template('admin.html')
+
+    else:
+        if not request.form.get("title"):
+            return render_template("admin.html", message = "Title Missing")
+        if not request.form.get("name"):
+            return render_template("admin.html", message = "Name Missing")
+        # if not request.form.get("filename"):
+        #     return render_template("admin.html", message = "File Missing")
+        if not request.form.get("content"):
+            return render_template("admin.html", message = "Content Missing")
+        file = request.files['filename']
+        print(file)
+        filename = secure_filename(file.filename)
+        file.save(filename)
+
+        time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+       
+        # print(request.form.get("filename"))
+        # db.execute("INSERT INTO blog (title, user_id, date, content, img, name) VALUES(:title, :user_id, :date, :content, :img, :name)", {
+        #     'title': request.form.get("title"), 'user_id': session["user_id"], 'date': time, 'content': request.form.get("content"), 
+        # })
+
+        return redirect('/admin')
+        
+        
+
+        
 @app.route('/blog')
 def blog():
     return render_template('blog.html')
