8985b3443d53f460fdfc3e70388b51bf7dd4b69a
Sahil Verma <iamsahil1910@gmail.com>
2021-12-10 23:08:20 +0530

    fixed for prod

--- app.py
+++ app.py
@@ -13,9 +13,9 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get('DATABASE_URL')
 # Set up database
-engine = create_engine("postgresql:///temp")
+engine = create_engine(os.environ.get('DATABASE_URL'))
 db = scoped_session(sessionmaker(bind=engine))
 
 
--- import.py
+++ import.py
@@ -1,8 +1,8 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 
-
-engine = create_engine("postgresql:///temp")
+engine = create_engine(os.environ.get('DATABASE_URL'))
 db = scoped_session(sessionmaker(bind=engine))
 
 def main():
