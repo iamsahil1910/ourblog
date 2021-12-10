4afcb131ab5045881d551e5917e3f494a30f3b55
Sahil Verma <iamsahil1910@gmail.com>
2021-12-10 23:12:41 +0530

    fixed gunicorn

--- app.py
+++ app.py
@@ -367,7 +367,8 @@ def handle_404(e):
 
     
 if(__name__ == "__main__"):
-    app.run(host="0.0.0.0")
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host="0.0.0.0", port=port)
     
 
 
