e6eaf005852fa28c52c09c8fd38c9b67166e19f5
Sahil Verma <iamsahil1910@gmail.com>
2021-12-10 19:47:18 +0530

    upload image, edit image, blog without image, change blog name so that it don't interfare

--- import.py
+++ import.py
@@ -9,7 +9,7 @@ def main():
     # Create table to import data into
     db.execute("CREATE TABLE admin (user_id  SERIAL PRIMARY KEY, username VARCHAR(30), password VARCHAR(30))")
     db.execute("CREATE TABLE blog (blog_id SERIAL PRIMARY KEY, title VARCHAR NOT NULL, user_id INTEGER, date DATE, content VARCHAR, img VARCHAR, name VARCHAR NOT NULL)")
-    db.execute("CREATE TABLE comments (id SERIAL PRIMARY KEY, name VARCHAR NOT NULL, comment VARCHAR NOT NULL, blog_id INTEGER NOT NULL REFERENCES blog(blog_id) ON DELETE CASCADE)")
+    db.execute("CREATE TABLE comments (id SERIAL PRIMARY KEY, name VARCHAR NOT NULL, comment VARCHAR NOT NULL, date DATE, blog_id INTEGER NOT NULL REFERENCES blog(blog_id) ON DELETE CASCADE)")
     db.execute("CREATE TABLE likes (id INT PRIMARY KEY, likes_count INTEGER)")
 
     db.execute("INSERT INTO admin (username, password) VALUES(:username, :password)", {'username': 'admin', 'password': 'admin'})
