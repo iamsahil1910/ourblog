e32a93305ab1c958bd99b01baac32f2e6dd06ac1
Sahil Verma <iamsahil1910@gmail.com>
2021-12-10 22:21:38 +0530

    setup.sh

---
+++ setup.sh
@@ -0,0 +1,3 @@
+export DATABASE_URL='postgres://kaejzqkhgeeidp:d31529dffb2475646f4bdcea98ea2dfe241a08f612be1ab28e6b16d5bfe37f87@ec2-184-73-25-2.compute-1.amazonaws.com:5432/de2m644fh9o2r7
+'
+python3 import.py
